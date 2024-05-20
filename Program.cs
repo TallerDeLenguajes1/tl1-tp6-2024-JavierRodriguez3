3ea19a0412f917f10328c3a53dba1ec48884fb1f
JavierRodriguez3 <javrodriguez2003@gmail.com>
2024-05-20 20:01:23 -0300

    tarea 3

--- Program.cs
+++ Program.cs
@@ -29,50 +29,90 @@ Console.WriteLine($"El numero ingresado es: {aux2} y el numero invertido es: {ac
 
 //Ejercicio 2 
 
-/* int num1 = 0;
+int num1 = 0;
 int num2 = 0;
 int opcion = 0;
-
+double num3 = 0;
 do {
     Console.WriteLine("Ingresar 1 si desear sumar");
     Console.WriteLine("Ingresar 2 si desear restar");
     Console.WriteLine("Ingresar 3 si desear multiplicar");
     Console.WriteLine("Ingresar 4 si desear dividir");
+    Console.WriteLine("Ingresar 5 si desea el valor absoluto de un numero");
+    Console.WriteLine("Ingresar 6 si desea elevar el numero al cudrado");
+    Console.WriteLine("Ingresar 7 si desea la raiz cuadrada del numero");
+    Console.WriteLine("Ingresar 8 si desea el seno del numero");
+    Console.WriteLine("Ingresar 9 si desea el coseno del numero");
+    Console.WriteLine("Ingresar 10 si desea la parte entera de un numero entero");
     int operacion = Convert.ToInt32(Console.ReadLine());
     switch (operacion)
     {
         case 1:
-            Console.WriteLine("Ingresar pimer numero");
+            Console.WriteLine("Ingresar primer numero");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingresar segundo numero");
             num2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("La suma es: " + (num1 + num2));
             break;
         case 2:
-            Console.WriteLine("Ingresar pimer numero");
+            Console.WriteLine("Ingresar primer numero");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingresar segundo numero");
             num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("La suma es: " + (num1 - num2));
+            Console.WriteLine("La resta es: " + (num1 - num2));
             break;
         case 3:
-            Console.WriteLine("Ingresar pimer numero");
+            Console.WriteLine("Ingresar primer numero");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingresar segundo numero");
             num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("La suma es: " + (num1 * num2));
+            Console.WriteLine("La multiplicacion es: " + (num1 * num2));
             break;
         case 4:
-            Console.WriteLine("Ingresar pimer numero");
+            Console.WriteLine("Ingresar primer numero");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingresar segundo numero");
             num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("La suma es: " + (num1 / num2));
+            Console.WriteLine("La division es: " + (num1 / num2));
+            break;
+        case 5:
+            Console.WriteLine("Ingresar el numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            int va = Math.Abs(num1);
+            Console.WriteLine("El valor absoluto es: " + (va));
+            break;
+        case 6:
+            Console.WriteLine("Ingresar el nuemero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            double cuadrado = Math.Pow(num1, 2);
+            Console.WriteLine("El cudrado del numero es: "+ cuadrado);
+            break;
+        case 7:
+            Console.WriteLine("Ingresar el nuemero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            double raiz = Math.Sqrt(num1);
+            Console.WriteLine("La raiz del numero es: "+ raiz);
+            break;
+        case 8:
+            Console.WriteLine("Ingresar el nuemero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            double seno = Math.Sin(num1);
+            Console.WriteLine("El seno del numero es: "+ seno);
+            break;
+        case 9:
+            Console.WriteLine("Ingresar el nuemero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            double coseno = Math.Cos(num1);
+            Console.WriteLine("El seno del numero es: "+ coseno);
+            break;
+        case 10:
+            Console.WriteLine("Ingresar el numero");
+            double.TryParse(Console.ReadLine(), out num3);
+            double entera = Math.Floor(num3);
+            Console.WriteLine("La parte entera del numero es: " + entera);
             break;
     }
 
     Console.WriteLine("Ingresar 1 si desea seguir operando, ingresar 0 si ya no desea operar");
-    
-}while(opcion == 1); */
-
-//Ejercicio 3
+    opcion = Convert.ToInt32(Console.ReadLine());
+}while(opcion == 1); 
